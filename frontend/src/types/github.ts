0c868c56dd7028610e8173e45d35bdf24f782043
Stefan Anevski <anevskistefan11@gmail.com>
2024-12-03 21:07:53 +0100

    implement checkbox to filter assigned/not assigned issues

--- frontend/src/types/github.ts
+++ frontend/src/types/github.ts
@@ -38,6 +38,7 @@ export interface IssueParams {
   state: 'open' | 'closed';
   page: number;
   timeFrame?: 'day' | 'week' | 'month' | 'year' | 'all';
+  unassigned?: boolean;
 }
 export interface Activity {
   id: string;
