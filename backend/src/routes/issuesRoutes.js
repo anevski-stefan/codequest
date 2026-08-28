3efad219890b5131c7a707caf8688d074ceafe62
Stefan Anevski <105498279+anevski-stefan@users.noreply.github.com>
2026-08-28 11:13:36 +0200

    remove dead backend issue list endpoint

--- backend/src/routes/issuesRoutes.js
+++ backend/src/routes/issuesRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getIssues,
   getAssignedIssues
 } = require('../controllers/issuesController');
 const etagMiddleware = require('../middleware/etagMiddleware');
-router.get('/', etagMiddleware, getIssues);
 router.get('/assigned', etagMiddleware, getAssignedIssues);
 module.exports = router;
