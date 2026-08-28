92b830358db2b31bd3c47822d1bab2c16f5435f1
Stefan Anevski <105498279+anevski-stefan@users.noreply.github.com>
2026-08-28 11:44:10 +0200

    fold comments routes into single issuesRouter, remove commentsRoutes

--- backend/src/routes/commentsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {
-  getIssueComments
-} = require('../controllers/commentsController');
-const etagMiddleware = require('../middleware/etagMiddleware');
-router.get('/:issueNumber/comments', etagMiddleware, getIssueComments);
-module.exports = router;
--- backend/src/routes/issuesRoutes.js
+++ backend/src/routes/issuesRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const {
   getAssignedIssues
 } = require('../controllers/issuesController');
+const {
+  getIssueComments
+} = require('../controllers/commentsController');
 const etagMiddleware = require('../middleware/etagMiddleware');
 router.get('/assigned', etagMiddleware, getAssignedIssues);
+router.get('/:issueNumber/comments', etagMiddleware, getIssueComments);
 module.exports = router;
